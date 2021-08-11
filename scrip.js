083af84f0f035eba33a06ee9896a4b5b5d8dfda0
Gustavo Morel <gustavo.d.morel@gmail.com>
2021-08-11 18:58:01 -0300

    objeto agregado

--- scrip.js
+++ scrip.js
@@ -31,6 +31,8 @@ let aforocentenario = parseInt(ecuacion(centenario))
 
 
 
+
+
 while (estadio != 4) {
 
     switch (estadio) {
@@ -52,13 +54,41 @@ while (estadio != 4) {
             }
                 else {
 
-            let cantEntradascds = parseInt(prompt(compradorCDS + " ingrese cantidad de entradas:"))
-            console.log(compradorCDS + " compro para el estadio CDS: " + cantEntradascds)
+        class entrada {
+        constructor(precio, descuento, cantidad) {
+        this.precio  = 250;
+        this.descuento  = parseFloat(descuento);
+        this.cantidad = cantidad;
+        }
+    
+        compra(){
+            this.venta = (this.precio - this.descuento) * this.cantidad;
+        }
+        
+        ahorro() {
+            this.descuento = this.venta; 
+        }
 
+        visualizar(){
+            console.log(this)
+            console.log(compradorCDS + " compro para el estadio CDS: " + cantEntradascds)
+        }
+
+        }
+
+        const comprador1 = new entrada (this.precio,prompt("Ingrese descuento: Socios $20 descuento / No socios Ingrese 0"),parseInt(prompt(compradorCDS + " ingrese cantidad de entradas:")))
+        let cantEntradascds = comprador1.cantidad
+        
+        comprador1.compra();
+        comprador1.ahorro(),
+
+            alert("Total a pagar: " + "$" + comprador1.descuento)
             alert("Gracias por su compra , aun quedan para el estadio " + est1 + " " + entradas(aforocds, cantEntradascds))
 
+        comprador1.visualizar(); 
             break
-            
+          
+
             }
 
             //Gran Parque Central
@@ -77,15 +107,45 @@ while (estadio != 4) {
                 alert("Menores de edad no estan autorizados a comprar entradas para este tipo de eventos")
                 
                 break
-            }
+                }
                 else {
-            let cantEntradasgpc = parseInt(prompt(compradorGPC + " ingrese cantidad de entradas:"))
-            console.log(compradorGPC + " compro para el estadio GPC: " + cantEntradasgpc)
 
-            alert("Gracias por su compra , aun quedan para el estadio " + est2 + " " + entradas(aforogpc, cantEntradasgpc))
+                    class entrada {
+                        constructor(precio, descuento, cantidad) {
+                        this.precio  = 250;
+                        this.descuento  = parseFloat(descuento);
+                        this.cantidad = cantidad;
+                        }
+                    
+                        compra(){
+                            this.venta = (this.precio - this.descuento) * this.cantidad;
+                        }
+                        
+                        ahorro() {
+                            this.descuento = this.venta; 
+                        }
+                
+                        visualizar(){
+                            console.log(this)
+                            console.log(compradorGPC + " compro para el estadio GPC: " + cantEntradasgpc)
+                        }
+                
+                        }
+                
+                        const comprador1 = new entrada (this.precio,prompt("Ingrese descuento: Socios $20 descuento / No socios Ingrese 0"),parseInt(prompt(compradorGPC + " ingrese cantidad de entradas:")))
+                        let cantEntradasgpc = comprador1.cantidad
+                        
+                        comprador1.compra();
+                        comprador1.ahorro(),
+                    
+            
+            
+                    alert("Total a pagar: " + "$" + comprador1.descuento)
+                    alert("Gracias por su compra , aun quedan para el estadio " + est2 + " " + entradas(aforogpc, cantEntradasgpc))
 
-            break
-            }
+                    comprador1.visualizar(); 
+                    break
+                    }
 
             //Centenario
         case 3:
@@ -104,13 +164,43 @@ while (estadio != 4) {
                 break
             }
                 else {
-            let cantEntradascen = parseInt(prompt(compradorCEN + " ingrese cantidad de entradas:"))
-            console.log(compradorCEN + " compro para el estadio Centenario: " + cantEntradascen)
 
-            alert("Gracias por su compra , aun quedan para el estadio " + est3 + " " + entradas(aforocentenario, cantEntradascen))
+                    class entrada {
+                        constructor(precio, descuento, cantidad) {
+                        this.precio  = 250;
+                        this.descuento  = parseFloat(descuento);
+                        this.cantidad = cantidad;
+                        }
+                    
+                        compra(){
+                            this.venta = (this.precio - this.descuento) * this.cantidad;
+                        }
+                        
+                        ahorro() {
+                            this.descuento = this.venta; 
+                        }
+                
+                        visualizar(){
+                            console.log(this)
+                            console.log(compradorCEN + " compro para el estadio Centenario: " + cantEntradascen)
+                        }
+                
+                        }
 
-            break
-            }
+                        const comprador1 = new entrada (this.precio,prompt("Ingrese descuento: Socios $20 descuento / No socios Ingrese 0"),parseInt(prompt(compradorCEN + " ingrese cantidad de entradas:")))
+
+                        let cantEntradascen = comprador1.cantidad
+
+                        comprador1.compra();
+                        comprador1.ahorro(),
+                    
+            
+                        alert("Total a pagar: " + "$" + comprador1.descuento)
+                        alert("Gracias por su compra , aun quedan para el estadio " + est3 + " " + entradas(aforocentenario, cantEntradascen))
+
+                        comprador1.visualizar();
+                        break
+                        }
 
             //Default
         default:
@@ -128,4 +218,4 @@ while (estadio != 4) {
 }
 
 
-console.log("FUERA")
+console.log("FUERA DEL PROGRAMA MIMIX - 1.0")
